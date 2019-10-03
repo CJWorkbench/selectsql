8412da04f7334c930fa0860e2d1ed3a4d1425da2
Adam Hooper <adam@adamhooper.com>
2019-10-03 14:47:44 -0400

    migrate_params(): nix "run" param

--- selectsql.py
+++ selectsql.py
@@ -5,35 +5,49 @@ from pandas.io.sql import DatabaseError
 
 def sqlselect(table, sql):
     if len(table.columns) == 0:
-        return (pandas.DataFrame(), '')
+        return (pandas.DataFrame(), "")
 
-    with sqlite3.connect(':memory:') as conn:
-        table.to_sql('input', conn, index=False)
+    with sqlite3.connect(":memory:") as conn:
+        table.to_sql("input", conn, index=False)
 
         try:
             dataframe = pandas.read_sql_query(sql, conn)
         except DatabaseError as err:
             message = str(err)
 
-            expect_start = f'Execution failed on sql \'{sql}\': '
+            expect_start = f"Execution failed on sql '{sql}': "
             if message.startswith(expect_start):
-                message = message.replace(expect_start, 'SQL error: ', 1)
+                message = message.replace(expect_start, "SQL error: ", 1)
 
-            if message.startswith('SQL error: near '):
-                message = message.replace('SQL error: near ',
-                                          'SQL error near ', 1)
+            if message.startswith("SQL error: near "):
+                message = message.replace("SQL error: near ", "SQL error near ", 1)
 
-            if 'no such table: ' in message:
+            if "no such table: " in message:
                 message += '\n\nThe only valid table name is "input"'
 
             return (None, message)
 
-        return (dataframe, '')
+        return (dataframe, "")
 
 
 def render(table, params):
-    sql = params['sql']
+    sql = params["sql"]
     if not sql.strip():
-        return (None, 'Missing SQL SELECT statement')
+        return (None, "Missing SQL SELECT statement")
 
     return sqlselect(table, sql)
+
+
+def _migrate_params_v0_to_v1(params):
+    """
+    v0 had an empty 'run' param (it was a value generated from a button).
+
+    v1 has no 'run' param.
+    """
+    return {"sql": params["sql"]}
+
+
+def migrate_params(params):
+    if "run" in params:
+        params = _migrate_params_v0_to_v1(params)
+    return params
--- tests/test_selectsql.py
+++ tests/test_selectsql.py
@@ -1,45 +1,61 @@
 import unittest
 from pandas import DataFrame
 from pandas.testing import assert_frame_equal
-from selectsql import render
+from selectsql import migrate_params, render
 
 
-class TestSqlselect(unittest.TestCase):
+class MigrateParamsTest(unittest.TestCase):
+    def test_v0(self):
+        self.assertEqual(
+            migrate_params({"run": "", "sql": "SELECT * FROM input"}),
+            {"sql": "SELECT * FROM input"},
+        )
+
+    def test_v1(self):
+        self.assertEqual(
+            migrate_params({"sql": "SELECT * FROM input"}),
+            {"sql": "SELECT * FROM input"},
+        )
+
+
+class RenderTest(unittest.TestCase):
     def test_happy_path(self):
-        df = DataFrame({'foo': [1, 2, 3], 'bar': [2, 3, 4]})
-        result = render(df, {'sql': 'SELECT foo + bar AS baz FROM input'})
-        expected = (DataFrame({'baz': [3, 5, 7]}), '')
+        df = DataFrame({"foo": [1, 2, 3], "bar": [2, 3, 4]})
+        result = render(df, {"sql": "SELECT foo + bar AS baz FROM input"})
+        expected = (DataFrame({"baz": [3, 5, 7]}), "")
 
         self.assertEqual(result[1], expected[1])
         self.assertTrue(result[0].equals(expected[0]))
 
     def test_empty_sql(self):
-        df = DataFrame({'foo': [1, 2, 3]})
-        result = render(df, {'sql': ''})
+        df = DataFrame({"foo": [1, 2, 3]})
+        result = render(df, {"sql": ""})
         self.assertIsNone(result[0])
-        self.assertEqual(result[1], 'Missing SQL SELECT statement')
+        self.assertEqual(result[1], "Missing SQL SELECT statement")
 
     def test_invalid_sql_syntax(self):
-        df = DataFrame({'foo': [1, 2, 3]})
-        result = render(df, {'sql': 'This is not SQL'})
+        df = DataFrame({"foo": [1, 2, 3]})
+        result = render(df, {"sql": "This is not SQL"})
         self.assertIsNone(result[0])
         self.assertEqual(result[1], 'SQL error near "This": syntax error')
 
     def test_hint_invalid_table_name(self):
-        df = DataFrame({'foo': [1, 2, 3]})
-        result = render(df, {'sql': 'SELECT * FROM input2'})
+        df = DataFrame({"foo": [1, 2, 3]})
+        result = render(df, {"sql": "SELECT * FROM input2"})
         self.assertIsNone(result[0])
         self.assertEqual(
             result[1],
-            ('SQL error: no such table: input2'
-             '\n\nThe only valid table name is "input"')
+            (
+                "SQL error: no such table: input2"
+                '\n\nThe only valid table name is "input"'
+            ),
         )
 
     def test_empty_input(self):
         df = DataFrame()
-        result = render(df, {'sql': 'SELECT * FROM input'})
+        result = render(df, {"sql": "SELECT * FROM input"})
         assert_frame_equal(result[0], DataFrame())
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
